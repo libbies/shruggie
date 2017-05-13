f5f6d72b252a41ad02ac51dfc2fccbd71bb2cf6e
libbies <bcw@well.com>
2017-05-13 01:13:36 +0000

    rewrote some error messages; more tidying up

--- config.py
+++
@@ -1,9 +0,0 @@
-# get a token for your bot here: https://discordapp.com/developers/applications/me
-SHRUGGIE_APP_TOKEN='APP BOT USER TOKEN GOES HERE'
-
-# the bot's initial nick name
-# TODO: store the bot's current nick name somewhere, for when the bot reloads
-SHRUGGIE_NICK_NAME="┐('～')┌"
-
-# the log file directory
-SHRUGGIE_LOG_DIR='/home/_shruggie/logs'
