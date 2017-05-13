b939fdc9f326da262fae3a6127ae36dd12a9741c
libbies <8513169+libbies@users.noreply.github.com>
2017-05-13 00:34:48 +0000

    misc. bug fixes

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # get a token for your bot here: https://discordapp.com/developers/applications/me
 SHRUGGIE_APP_TOKEN='APP BOT USER TOKEN GOES HERE'
-SHRUGGIE_APP_TOKEN='MzExNjM2NzEzODI0ODQ1ODI2.C_fLcg.MYKSdqil6rUqkptP_DkdhP0uPRU'
 
 # the bot's initial nick name
 # TODO: store the bot's current nick name somewhere, for when the bot reloads
