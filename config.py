16094cfb080974184bf78561d1f746c2c05f33a0
libbies <8513169+libbies@users.noreply.github.com>
2017-05-15 01:58:37 +0000

    added config options for command prefixes

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ SHRUGGIE_APP_TOKEN='APP BOT USER TOKEN GOES HERE'
 
 # the bot's initial nick name
 # TODO: store the bot's current nick name somewhere, for when the bot reloads
-SHRUGGIE_NICK_NAME="┐('～')┌"
+SHRUGGIE_NICKNAME="┐('～')┌"
 
 # the log file directory
 SHRUGGIE_LOG_DIR='/home/_shruggie/logs'
 
 # log level
 SHRUGGIE_LOG_LVL='WARNING'
+
+# command prefix 
+# TODO: save/load config dynamically
+SHRUGGIE_CMD_PREFIX='!'
